7e0fbc3be318e8e9796ef3fb4d76a2772af75714
Dang Kieu <dangkieu@b13technology.com>
2021-03-27 01:19:58 +0700

    add swagger
    1

--- index.ts
+++ index.ts
@@ -1,12 +1,45 @@
-import Hapi from "@hapi/hapi"
-import Jwt from "@hapi/jwt"
-import glob from "glob"
-import path from "path"
-import { JWT_AUD, JWT_ISS, JWT_SECRET_KEY } from "./src/configs"
+import Hapi, { ServerRegisterPluginObject } from "@hapi/hapi";
+import Inert from '@hapi/inert';
+import Jwt from "@hapi/jwt";
+import Vision from '@hapi/vision';
+import glob from "glob";
+import HapiSwagger, { RegisterOptions } from 'hapi-swagger';
+import path from "path";
+import { JWT_AUD, JWT_ISS, JWT_SECRET_KEY } from "./src/configs";
+
 
 async function initServer() {
   const server = Hapi.server({ port: process.env.PORT || 8000 })
-  await server.register(Jwt)
+  const swaggerOptions: RegisterOptions = {
+    documentationPath: '/docs',
+    info: {
+      title: 'Test API Documentation',
+    },
+    securityDefinitions: {
+      'jwt': {
+        'type': 'apiKey',
+        'name': 'Authorization',
+        'in': 'header',
+      }
+    },
+    security: [{ jwt: [] }]
+  };
+
+  const plugins: Array<ServerRegisterPluginObject<any>> = [
+    Jwt,
+    {
+      plugin: Inert
+    },
+    {
+      plugin: Vision
+    },
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions
+    }
+  ];
+
+  await server.register(plugins);
 
   server.auth.strategy("jwt", "jwt", {
     keys: JWT_SECRET_KEY,
@@ -23,21 +56,21 @@ async function initServer() {
       return {
         isValid: true,
         scope: { scope: artifacts.decoded.payload.scope },
-      }
+      };
     },
-  })
+  });
 
   glob
     .sync("dist/src/routes/**/*.js", {
       root: __dirname,
     })
     .forEach((file) => {
-      const route = require(path.join(__dirname, "../", file))
-      server.route(route)
-    })
+      const route = require(path.join(__dirname, "../", file));
+      server.route(route);
+    });
 
-  await server.start()
-  console.log("server running at: " + server.info.uri)
+  await server.start();
+  console.log("server running at: " + server.info.uri);
 }
 
-initServer()
+initServer();
--- src/routes/posts/get-post-by-id.ts
+++ src/routes/posts/get-post-by-id.ts
@@ -1,11 +1,14 @@
 import { Boom } from "@hapi/boom"
+import Joi from "joi"
 import { routes } from "../../constants/routes"
 import { postGateway } from "../../gateways"
 
 const getPostById = {
   method: "GET",
   path: routes.posts.detail.value,
-  config: {
+  options: {
+    tags: ['api'],
+    description: 'Get post by id',
     handler: async function (request, h): Promise<object> {
       const posts = await postGateway.getPosts()
 
@@ -18,11 +21,18 @@ const getPostById = {
 
       throw new Boom("Post Not Found", { statusCode: 404 })
     },
+    validate: {
+      params: Joi.object({
+        id: Joi.number()
+          .required()
+          .description('the id for post'),
+      })
+    },
     auth: {
       strategy: "jwt",
       scope: ["admin"],
     },
-  },
+  }
 }
 
 module.exports = getPostById
